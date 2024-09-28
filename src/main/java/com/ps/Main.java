a844b149aee52064c30fb489db81b72936439dac
Debani <debani.cossio1217@gmail.com>
2024-09-28 17:24:21 -0500

    thought process

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,27 @@
 package com.ps;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    static Scanner scanner = new Scanner(System.in);
+
+    // Calculator 1: Mortgage Calculator
+// calculate how much a monthly payment for a loan (minus insurance/taxes)?
+// how much interest you would pay over the life of the loan?
+
+   // Inputs
+// amountOfLoan - original sum of the loan borrowed, double
+// annualInterestRate - yearly interest rate, float
+// annualLoanLength - yearly loan length, short
+
+   // Outputs
+// totalMonthlyPayments = paymentsPerYear/12 = paymentsPerYear, byte
+// monthlyInterestRate = annualInterestRate/12/100, double
+// totalInterest, double
+
+
+
     }
 }
