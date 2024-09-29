1f23a3b4ad392830b169537f255e1f37b499bbd4
Debani <debani.cossio1217@gmail.com>
2024-09-29 00:55:22 -0500

    added calculations

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,11 +1,8 @@
 package com.ps;
-import java.util.*;
+
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-    static Scanner scanner = new Scanner(System.in);
 
     // Calculator 1: Mortgage Calculator
 // calculate how much a monthly payment for a loan (minus insurance/taxes)?
@@ -18,10 +15,39 @@ public class Main {
 
    // Outputs
 // totalMonthlyPayments = paymentsPerYear/12 = paymentsPerYear, byte
-// monthlyInterestRate = annualInterestRate/12/100, double
+// monthlyInterestRate = annualInterestRate/12/100, float
 // totalInterest, double
 
+        static Scanner scanner = new Scanner(System.in);
 
+        public static void main(String[] args) {
+            System.out.println("Welcome to Mortgage Calculator");
+
+            System.out.println("Please enter loan amount: ");
+            double amountOfLoan = scanner.nextDouble();
+
+            System.out.println("Please enter annual interest rate (as percentage): ");
+            float annualInterestRate = scanner.nextFloat();
+
+            System.out.println("Please enter loan term in years: ");
+            short annualLoanLength = scanner.nextShort();
+
+      // Calculate the monthlyInterestRate
+        float monthlyInterestRate = annualInterestRate / 12 / 100;
+
+      // Convert the annualLoanLength into months
+        int totalMonthlyPayments = annualLoanLength * 12;
+
+      // Calculate the monthlyPayment
+        double monthlyPayment = amountOfLoan * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, - totalMonthlyPayments)));
+
+      // Calculate the totalInterest
+        double totalInterest = (monthlyPayment * totalMonthlyPayments) - amountOfLoan;
+
+      // Display results/System.out.println();
+        System.out.printf("Your monthly payment will be: $%.2f");
+
+        System.out.printf("The total interest you will pay over your annual loan length: $%.2f");
 
     }
 }
